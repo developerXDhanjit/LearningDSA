8f06b2e088f21045b7700c14d45628771981dba5
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-30 13:19:34 +0530

    added some on logarithms

---
+++ basics/05_logarithm.js
@@ -0,0 +1,9 @@
+/* Greater than or equal to 2 , binary log  */
+
+/* logarithm time complexity is great !  */
+
+/* O(log n ) is fantastic  */
+/* O(nlog n ) is also better than O(n^2 complexity) */
+
+/* why to learn , cause some algorithm has these log space complexity */
+
