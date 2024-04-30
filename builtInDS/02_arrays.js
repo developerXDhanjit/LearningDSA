996c6cc2f8f96331e13b2b343922d0b0a75d7247
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-30 13:55:41 +0530

    added big o of arrays

---
+++ builtInDS/02_arrays.js
@@ -0,0 +1,21 @@
+/* ordered lists of things  */
+/* use when you need order   */
+
+/* Big O of arrays  
+  insertion - it depends 
+  removal - it depends 
+  searching - O(n)
+  access - O(1) //superfast wow 
+
+ Ways to insert / remove :-
+ 1) first way {FILO}
+    adding at the end -> push -> O(1) 
+    removing from the end -> pop -> O(1)
+
+ 2) Second way {FIFO}
+    avoid , if not neccessary 
+    adding at the first -> shift -> O(n)
+    removing from the beginning -> unshift -> O(n)
+    due to the change in the index 
+
+*/
