3b7ea25aaa5ffd54089f28df95dc4ed9c58a2389
developerXDhanjit <developerdhanjit@gmail.com>
2024-04-30 13:21:56 +0530

    some changes due to an error

--- basics/04_spaceComplexity.js
+++ basics/04_spaceComplexity.js
@@ -5,3 +5,11 @@ analyze the complexity taken by the memory , big O can still be used
 We will discuss about auxilary space complexity , meaning only the amount of space taken by the algorithm
 
 */
+
+/* Rules of Thumb 
+
+1. booleans , numbers , undefined are constant space
+2. Strings require O(n) space
+3. Reference types generally take O(n) , n is length or keys of objects 
+
+*/
